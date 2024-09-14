57e14f641329fa1cb7b47594b7ad62247557663f
Chloe Ta <cqta@ucsb.edu>
2024-09-14 16:13:13 -0700

    initial file upload component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import { DarkThemeToggle } from "flowbite-react";
+import { FileDropzone } from "./components/FileDropzone"; // Adjust the path based on your folder structure
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-2 dark:bg-gray-800">
       <h1 className="text-2xl dark:text-white">Flowbite React + Next.js</h1>
       <DarkThemeToggle />
+      <FileDropzone /> {/* Integrate the FileDropzone component here */}
     </main>
   );
 }
